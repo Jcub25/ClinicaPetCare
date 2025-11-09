f869410ab11a2f4bf66fbf9770bd9fae4d7204cb
Jeanca <jcubillo701@gmail.com>
2025-11-08 21:59:19 -0600

    Creamos la estructura que simula el SQL para guardar los datos en una lista y para mostrar los ingresados

--- Data/PetRepo.cs
+++ Data/PetRepo.cs
@@ -1,6 +1,19 @@
-﻿namespace ClinicaPetCare.Data
+﻿using ClinicaPetCare.Models;
+
+namespace ClinicaPetCare.Data
 {
     public class PetRepo
     {
+        // Creo la lista temporal para guardar los datos ingresados
+        private static readonly List<Pet> _pets = new();
+
+        // Devolvemos todas las mascotas registradas
+        public static IReadOnlyList<Pet> ObtenerTodos() => _pets.AsReadOnly();
+
+        // Agrega una nueva mascota
+        public static void Agregar(Pet mascota)
+        {
+            _pets.Add(mascota);
+        }
     }
 }
