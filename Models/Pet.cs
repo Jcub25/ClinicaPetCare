070d565bcb92151ef9e5f4487ec190bfe7703d1f
Jeanca <jcubillo701@gmail.com>
2025-11-09 21:16:16 -0600

    Se trabaja el modelo Pet.cs, para ajustar el nombre que se muestra en la parte web en cada campo del formulario, y se ajustan validaciones luego de pruebas, en el Layout de views se crea el boton mascotas para poder accesar desde la pagina principal al formulario y no mediante el cambio desde la URL.

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -12,30 +12,36 @@ namespace ClinicaPetCare.Models
         [StringLength(50, MinimumLength = 2, ErrorMessage = "El nombre debe tener al menos 2 caracteres.")]
         public string NombreMascota { get; set; }
 
+        [Display(Name = "Seleccione la especie del animal")]
         [Required(ErrorMessage = "Debe seleccionar una especie.")]
-        public string Especie { get; set; } 
+        public string Especie { get; set; }
 
+        [Display(Name = "Escriba la raza del aninaml")]
         [Required(ErrorMessage = "La raza es obligatoria.")]
         public string Raza { get; set; }
 
+        [Display(Name = "Digite la edad del animal, utilice numeros enteros")]
         [Range(0, 25, ErrorMessage = "La edad debe estar entre 0 y 25 años.")]
         public int Edad { get; set; }
 
+        [Display(Name = "Escriba el nombre del dueño")]
         [Required(ErrorMessage = "El nombre del dueño es obligatorio.")]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre del dueño debe tener al menos 3 caracteres.")]
         public string NombreDueno { get; set; }
 
+        [Display(Name = "Digite el numero de telefono")]
         [Required(ErrorMessage = "El telefono del dueño es obligatorio.")]
         [RegularExpression(@"^\d{8}$", ErrorMessage = "El telefono debe tener exactamente 8 digitos.")]
         public string TelefonoDueno { get; set; }
 
+        [Display(Name = "Fecha de ingreso")]
         [Required(ErrorMessage = "La fecha de ingreso es obligatoria.")]
         [DataType(DataType.Date)]
         [FechaNoFutura(ErrorMessage = "La fecha de ingreso no puede ser futura.")]
         public DateTime FechaIngreso { get; set; }
     }
 
-    // 🔹 Validación personalizada para que la fecha no sea futura
+    //  Para validar que no ingresen una fecha futura
     public class FechaNoFuturaAttribute : ValidationAttribute
     {
         public override bool IsValid(object value)
